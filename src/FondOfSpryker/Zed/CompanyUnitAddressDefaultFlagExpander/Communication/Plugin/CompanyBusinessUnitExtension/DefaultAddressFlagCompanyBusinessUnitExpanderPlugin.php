fc12aea3c38b6fa3c65a4ae49ea5c8e3ff1e3f7b
Jozsef Geng <gengjozsef86@gmail.com>
2019-12-17 10:41:02 +0100

    set Default Address Bool Value in case is not a default address

--- src/FondOfSpryker/Zed/CompanyUnitAddressDefaultFlagExpander/Communication/Plugin/CompanyBusinessUnitExtension/DefaultAddressFlagCompanyBusinessUnitExpanderPlugin.php
+++ src/FondOfSpryker/Zed/CompanyUnitAddressDefaultFlagExpander/Communication/Plugin/CompanyBusinessUnitExtension/DefaultAddressFlagCompanyBusinessUnitExpanderPlugin.php
@@ -3,6 +3,7 @@
 namespace FondOfSpryker\Zed\CompanyUnitAddressDefaultFlagExpander\Communication\Plugin\CompanyBusinessUnitExtension;
 
 use Generated\Shared\Transfer\CompanyBusinessUnitTransfer;
+use Generated\Shared\Transfer\CompanyUnitAddressTransfer;
 use Spryker\Zed\CompanyBusinessUnitExtension\Dependency\Plugin\CompanyBusinessUnitExpanderPluginInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
@@ -19,24 +20,37 @@ class DefaultAddressFlagCompanyBusinessUnitExpanderPlugin extends AbstractPlugin
      */
     public function expand(CompanyBusinessUnitTransfer $companyBusinessUnitTransfer): CompanyBusinessUnitTransfer
     {
-        $defaultBillingAddress = $companyBusinessUnitTransfer->getDefaultBillingAddress();
+        $idDefaultBillingAddress = $companyBusinessUnitTransfer->getDefaultBillingAddress();
         $companyUnitAddressCollectionTransfer = $companyBusinessUnitTransfer->getAddressCollection();
 
-        if ($companyUnitAddressCollectionTransfer === null || $defaultBillingAddress === null) {
+        if ($companyUnitAddressCollectionTransfer === null) {
             return $companyBusinessUnitTransfer;
         }
 
         foreach ($companyUnitAddressCollectionTransfer->getCompanyUnitAddresses() as $companyUnitAddressTransfer) {
-            if ($companyUnitAddressTransfer->getIdCompanyUnitAddress() !== $defaultBillingAddress) {
-                continue;
-            }
-
-            $companyUnitAddressTransfer->setIsDefaultBilling(true)
-                ->setIsDefaultShipping(true);
-
-            break;
+            $this->setAddressBoolValue($companyUnitAddressTransfer, $idDefaultBillingAddress);
         }
 
         return $companyBusinessUnitTransfer;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CompanyUnitAddressTransfer $companyUnitAddressTransfer
+     * @param int $idDefaultBillingAddress
+     *
+     * @return \Generated\Shared\Transfer\CompanyUnitAddressTransfer
+     */
+    protected function setAddressBoolValue(
+        CompanyUnitAddressTransfer $companyUnitAddressTransfer,
+        int $idDefaultBillingAddress
+    ): CompanyUnitAddressTransfer {
+
+        if ($companyUnitAddressTransfer->getIdCompanyUnitAddress() === $idDefaultBillingAddress) {
+            return $companyUnitAddressTransfer->setIsDefaultBilling(true)
+                ->setIsDefaultShipping(true);
+        }
+
+        return $companyUnitAddressTransfer->setIsDefaultBilling(false)
+            ->setIsDefaultShipping(false);
     }
 }
